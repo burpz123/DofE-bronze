cadd98d4622132765f9bfd8aa6e0738043249b05
Eric Tyas <nkpoikana@gmail.com>
2019-09-01 17:59:28 +0100

    lesson 2

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ for dofe bronze
 ## Lesson 1 - 20 August 2019 (1 hour sesh)
 Introduction to github
 https://www.youtube.com/watch?v=0fKg7e37bQE
+## Lesson 2 - 01 september 2019 (2hr 30 sesh)
+added a picture to the website and added conditions to input box
---
+++ p1/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img v-bind:src='devilman'>    
+    <HelloWorld msg="Welcome"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+      },
+  data: function() {
+    return {
+      devilman: require('./assets/devilman.gif')
+    }
+  }  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
